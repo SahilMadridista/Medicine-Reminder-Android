3a1cf1d3bf7bdf47c7479111d3911b0a587a2c28
sahil <sahil91098singh@gmail.com>
2020-07-29 20:52:09 +0530

    UI almost done.

--- app/src/main/java/com/example/medicinereminder/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/MainActivity.java
@@ -1,14 +1,133 @@
 package com.example.medicinereminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+   private RelativeLayout DialogLayout,MainLayout;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+      ImageView BackImage;
+      FloatingActionButton FloatingButton;
+
+      DialogLayout = findViewById(R.id.dialog_layout);
+      MainLayout = findViewById(R.id.main_layout);
+      FloatingButton = findViewById(R.id.fab);
+      BackImage = findViewById(R.id.back_image);
+
+
+      FloatingButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            MedicineDialog();
+         }
+      });
+
+      BackImage.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            CloseMedicineDialog();
+         }
+      });
+
+   }
+
+   private void MedicineDialog() {
+
+      MainLayout.setVisibility(View.GONE);
+      DialogLayout.setVisibility(View.VISIBLE);
+
+      YoYo.with(Techniques.SlideInUp)
+              .repeat(0)
+              .duration(500)
+              .playOn(DialogLayout);
+
+   }
+
+   private void CloseMedicineDialog() {
+
+      YoYo.with(Techniques.SlideOutDown)
+              .duration(300)
+              .repeat(0)
+              .playOn(DialogLayout);
+
+      new Handler().postDelayed(new Runnable() {
+         @Override
+         public void run() {
+            DialogLayout.setVisibility(View.GONE);
+         }
+      },300);
+
+      MainLayout.setVisibility(View.VISIBLE);
+
+
+   }
+
+   @Override
+   public void onBackPressed() {
+
+      if(DialogLayout.getVisibility() == View.VISIBLE){
+
+         YoYo.with(Techniques.SlideOutDown)
+                 .duration(300)
+                 .repeat(0)
+                 .playOn(DialogLayout);
+
+         new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               DialogLayout.setVisibility(View.GONE);
+            }
+         },300);
+
+         MainLayout.setVisibility(View.VISIBLE);
+
+      }
+
+      else{
+
+         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         builder.setMessage("Do you really want to exit ?").setCancelable(false)
+                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                       MainActivity.super.onBackPressed();
+                       finish();
+
+                    }
+                 })
+
+                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                       dialogInterface.cancel();
+                    }
+                 });
+
+         AlertDialog alertDialog = builder.create();
+         alertDialog.show();
+
+      }
+
+
    }
 }
